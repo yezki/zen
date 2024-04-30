c310b36abec7c9f1c20d535f99d32cd124472ee1
Yeheizkiel Gabriel Batistuta <yeheizkielgb@gmail.com>
2024-04-30 16:38:41 +0700

    style: add latin and emoji fonts

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-5xl font-bold">A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</h1>
+    <h1 class="text-5xl font-bold">a b c d e f g h i j k l m n o p q r s t u v w x y z</h1>
+    <h1 class="text-5xl font-bold">0 1 2 3 4 5 6 7 8 9</h1>
+    <h1 class="text-5xl font-bold">~ ` ! # $ % ^ & * ( ) { } [ ] : ; " ' , . < > ? / \ |</h1>
+    <p>🥰💀✌️🌴🐢🐐🍄⚽🍻👑📸😬👀🚨🏡🕊️🏆😻🌟🧿🍀🎨🍜</p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: [
+    ["@nuxtjs/google-fonts", {
+      display: "swap",
+      families: {
+        "Exo 2": [400, 500, 600, 700],
+        "Noto Color Emoji": true
+      },
+      prefetch: true,
+      preconnect: true,
+      preload: true
+    }],
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt"
+  ],
   shadcn: {
     /**
       * Prefix for all the imported component
